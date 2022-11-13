76fc685c59d86d32fea676abd5f1070ed929deb3
Denyse-L <dluzon1@sfsu.edu>
2022-11-13 13:13:50 -0800

    Updated State to show instrument and visualizer

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -1,5 +1,7 @@
 // 3rd party
 import { List, Map } from 'immutable';
+import { BassInstrument } from './instruments/bass';
+import { MilkdropVisualizer } from './visualizers/Milkdrop';
 
 // project dependencies
 import { PianoInstrument } from './instruments/Piano';
@@ -15,13 +17,13 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument]);       // similar to Instrument[]
+const instruments = List([PianoInstrument, BassInstrument]);       // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-const visualizers = List([WaveformVisualizer]);    // similar to Visualizer[]
+const visualizers = List([WaveformVisualizer, MilkdropVisualizer]);    // similar to Visualizer[]
 
 
 /**
