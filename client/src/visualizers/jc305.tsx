92c815afd24a725edeb922917454a2aff3b8897f
jc305 <jchengg305@gmail.com>
2022-12-04 12:08:07 -0800

    Completed: Visualizer 2.0, updated colors curretly works with sound.

--- client/src/visualizers/jc305.tsx
+++ client/src/visualizers/jc305.tsx
@@ -7,17 +7,17 @@ import {Visualizer} from "../Visualizers";
 
 export const JC305Visualizer = new Visualizer(
     "jc305-visualizer",
-    (p5: P5, analyzer: Tone.Analyser) => {
+    (p5: P5, analyzer: Tone.Analyser,) => {
         const width = window.innerWidth / 2;
         const height = window.innerHeight / 2;
 
         p5.background('black');
         p5.angleMode("degrees");
-        p5.strokeWeight(1);
-        p5.stroke('yellow')
-        p5.line(0, height, width, height);
-        p5.fill('red');
-        p5.translate(width / 2, height / 2);
+        p5.strokeWeight(5);
+        p5.stroke('white');
+        p5.fill('blue');
+        p5.rotate(0.1);
+        p5.translate(width / 2.0, height / 2.0);
 
         const values = analyzer.getValue();
         // This is what draws the circle
@@ -27,14 +27,18 @@ export const JC305Visualizer = new Visualizer(
 
             // This will create all the shapes that will react to playback
             for (let j = 1; j <= 20000; j += 100 / 20) {
+
                 const amplitude = values[j] as number;
                 const r = p5.map(amplitude, -1, 1, 200, 200) / 1.5;
-                // this will make the circle pulsate to beat
+
+                // this will make the circle pulsate to beat and create a sunlike appearance
                 // const x = height / 2 + amplitude * height + r * Math.cos(j);
                 // let y = r * Math.sin(j) * i;
+
                 // this variation will create an X
                 const x = height / 2 + amplitude * height + r * Math.cos(j);
                 let y = r * Math.cos(j) * i;
+
                 p5.vertex(x, y);
             }
             p5.endShape();
