490bef3a0981ae79cfeb3e58bbf154080a3caffa
jc305 <jchengg305@gmail.com>
2022-12-04 11:15:55 -0800

    Completed: Visualizer 2.0, this variation is an X as opposed to a circle O

--- client/src/visualizers/jc305.tsx
+++ client/src/visualizers/jc305.tsx
@@ -5,8 +5,6 @@ import * as Tone from "tone";
 // project imports
 import {Visualizer} from "../Visualizers";
 
-
-
 export const JC305Visualizer = new Visualizer(
     "jc305-visualizer",
     (p5: P5, analyzer: Tone.Analyser) => {
@@ -16,22 +14,27 @@ export const JC305Visualizer = new Visualizer(
         p5.background('black');
         p5.angleMode("degrees");
         p5.strokeWeight(1);
-        p5.stroke('white');
+        p5.stroke('yellow')
+        p5.line(0, height, width, height);
         p5.fill('red');
         p5.translate(width / 2, height / 2);
 
         const values = analyzer.getValue();
+        // This is what draws the circle
         for (let i = -1; i <= 1; i += 2) {
 
             p5.beginShape();
-            for (let j = 0; j <= 180; j += 0.5) {
+
+            // This will create all the shapes that will react to playback
+            for (let j = 1; j <= 20000; j += 100 / 20) {
                 const amplitude = values[j] as number;
                 const r = p5.map(amplitude, -1, 1, 200, 200) / 1.5;
-
-
-                const x = height / 2 + amplitude * height + r * Math.cos(j); // this will make the circle pulsate to beat
-                const y = r * Math.sin(j) * i;
-
+                // this will make the circle pulsate to beat
+                // const x = height / 2 + amplitude * height + r * Math.cos(j);
+                // let y = r * Math.sin(j) * i;
+                // this variation will create an X
+                const x = height / 2 + amplitude * height + r * Math.cos(j);
+                let y = r * Math.cos(j) * i;
                 p5.vertex(x, y);
             }
             p5.endShape();
