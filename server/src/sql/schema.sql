80d2577b19ade7999ff950406df701904cd60355
Denyse-L <dluzon1@sfsu.edu>
2022-12-05 06:13:57 -0800

    Added Twinkle Little Star, and updated the app.db to reflect change

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -14,3 +14,6 @@ VALUES (2, 'Jingle Bells', 'E3 E3 E3 E3 E3 E3 E3 G5 C1 D2 E3 F4 F4 F4 F4 F4 E3 E
 
 INSERT INTO songs(id, song_title, notes)
 VALUES(3, 'AOT Opening S1', 'D4 D4 F4 E4 C4 C4 D4 D4 F4 E4 C4 A4 F4 G4 E4 F4 D4 E4 C4 A4 F4 G4 E4 F4 D4 E4 C4');
+
+INSERT INTO songs(id, song_title, notes)
+VALUES(4, 'Twinkle Little Star', 'C4 C4 G4 G4 A4 A4 G4 F4 F4 E4 E4 D4 D4 C4 G4 G4 F4 F4 E4 E4 D4 G4 G4 F4 F4 E4 E4 D4 C4 C4 G4 G4 A4 A4 G4 F4 F4 E4 E4 D4 D4 C4');
