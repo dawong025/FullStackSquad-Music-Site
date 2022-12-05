cf0b01533a3e1cec7f89c4242692954b279a721f
jc305 <jchengg305@gmail.com>
2022-12-04 23:44:58 -0800

    Updated and merged dawong025 branch with main

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -7,12 +7,9 @@ import { List, Map } from 'immutable';
 import { PianoInstrument } from './instruments/Piano';
 import { XylophoneInstrument } from './instruments/Xylophone';
 import { WaveformVisualizer } from './visualizers/Waveform';
-<<<<<<< HEAD
 import { JC305Visualizer } from './visualizers/jc305';
-=======
 import { dawong025Visualizer } from './visualizers/dawong025';
 import { TrumpetInstrument } from './instruments/dawong025';
->>>>>>> dawong025
 
 
 /** ------------------------------------------------------------------------ **
@@ -24,21 +21,15 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-<<<<<<< HEAD
-const instruments = List([PianoInstrument, XylophoneInstrument]);       // similar to Instrument[]
-=======
-const instruments = List([PianoInstrument, TrumpetInstrument]);       // similar to Instrument[]
->>>>>>> dawong025
+const instruments = List([PianoInstrument, TrumpetInstrument, XylophoneInstrument]);       // similar to Instrument[]
+
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-<<<<<<< HEAD
-const visualizers = List([WaveformVisualizer, JC305Visualizer]);    // similar to Visualizer[]
-=======
-const visualizers = List([WaveformVisualizer, dawong025Visualizer]);    // similar to Visualizer[]
->>>>>>> dawong025
+const visualizers = List([WaveformVisualizer, JC305Visualizer, dawong025Visualizer]);    // similar to Visualizer[]
+
 
 
 /**
