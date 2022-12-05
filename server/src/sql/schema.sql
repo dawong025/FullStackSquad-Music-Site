9939ca42e652ff0d5f2669f97da1b33a316b6c19
jc305 <jchengg305@gmail.com>
2022-12-05 00:32:56 -0800

    Updated songs for schema, added Darrens AOT

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -9,10 +9,8 @@ CREATE TABLE songs (
 INSERT INTO songs (id, song_title, notes) 
 VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4');
 
-<<<<<<< HEAD
 INSERT INTO songs (id, song_title, notes)
 VALUES (2, 'Mary had a Little Lamb', 'E3 D2 C1 D2 E3 E3 E3 D2 D2 D2 E3 E3 E3 E3 D2 C1 D2 E3 E3 E3 E3 D2 D2 E3 D2 C1');
-=======
+
 INSERT INTO songs(id, song_title, notes)
 VALUES(3, 'AOT Opening S1', 'D4 D4 F4 E4 C4 C4 D4 D4 F4 E4 C4 A4 F4 G4 E4 F4 D4 E4 C4 A4 F4 G4 E4 F4 D4 E4 C4');
->>>>>>> dawong025
